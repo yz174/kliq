c6e0df5b1cfaf10077987fdf806c9564785fcedc
Ujjwal Atri <ujjwalkrish082@gmail.com>
2026-02-21 21:36:59 +0530

    feat: configure image remote patterns and add proxy middleware

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.clerk.dev",
+      },
+      {
+        protocol: "https",
+        hostname: "uploadthing.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
